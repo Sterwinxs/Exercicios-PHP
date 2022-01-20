11933b3f8dfb0d56a2db343f66bd489e746eb052
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 09:00:38 -0300

    Update Exercicio 6.php

--- Exercicio 6.php
+++ Exercicio 6.php
@@ -1,4 +1,5 @@
 <?php 
+//Efetue um algoritmo PHP que receba quatro notas de um aluno, calcule e imprima a média aritmética das notas e a mensagem de aprovado para média superior ou igual a 7.0 ou a mensagem de reprovado para média inferior a 7.0
 $nota1= 8;
 $nota2= 7;
 $nota3= 9;
