7cfbeed57f724924a9ef7d52f0b01626b66fe8f4
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 19:46:33 -0300

    Add files via upload

---
+++ Exercicio 19.php
@@ -0,0 +1,14 @@
+<?php
+//Entrar com 3 números e imprimi-los em ordem decrescente (suponha números diferentes).
+$valor1= 9;
+$valor2= 4;
+$valor3= 7;
+
+echo $valor1;
+echo '<hr>';
+echo $valor3;
+echo '<hr>';
+echo $valor2;
+
+
+?>
