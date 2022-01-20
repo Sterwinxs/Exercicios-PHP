0204955ef1562f55ca3c28b265d91ba2a63fa7b2
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 10:43:20 -0300

    Update Exercicio 2.php

--- Exercicio 2.php
+++ Exercicio 2.php
@@ -1,5 +1,5 @@
 <?php
-//Efetue um algoritmo PHP que receba um valor e imprima o texto "o valor é maior ou igual a 10" caso isso seja verdade, senão imprima "o valor não é maior ou igual a 10"
+//Efetue um algoritmo PHP que receba um valor e imprima o texto "o valor é maior ou igual a 10" caso isso seja verdade, senão imprima "o valor não é maior ou igual a 10".
 
 $num= 6;
 
