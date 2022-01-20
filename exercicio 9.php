b1eadafeceb5e93e63194676910615e33f5e038a
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 09:03:19 -0300

    Update exercicio 9.php

--- exercicio 9.php
+++ exercicio 9.php
@@ -1,5 +1,7 @@
 <?php
-$num= 10;
+//Efetue um algoritmo em PHP que receba um valor qualquer e imprima os valores de 0 até o valor recebido, exemplo: • Valor recebido = 9 • Impressão do programa – 0 1 2 3 4 5 6 7 8 9
+
+$num= 9;
 
 for($i=0; $i <= $num; $i++){
     echo $i . ' ';
