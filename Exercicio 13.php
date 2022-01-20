0559e8cd07f0a0e3d7c06281ab9f02e4f2fc98b4
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 12:08:50 -0300

    Add files via upload

---
+++ Exercicio 13.php
@@ -0,0 +1,12 @@
+<?php
+//Efetue um algoritmo PHP que receba uma string, encontre o número total de caracteres desta e imprima todos os números que existem entre o e o número total, exemplo: string = “Gil Eduardo de Andrade” total_caracter = 22 Imprime: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+$nome= 'Gil Eduardo de Andrade' ;
+print '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22';
+
+
+
+	
+
+
+
+?>
