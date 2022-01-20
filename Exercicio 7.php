f64e5c845f6032b72793ae1aceb0c1117585ac64
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 09:01:27 -0300

    Update Exercicio 7.php

--- Exercicio 7.php
+++ Exercicio 7.php
@@ -1,4 +1,6 @@
 <?php 
+//Efetue um algoritmo PHP que receba a altura e o sexo de uma pessoa, calcule e imprima o seu peso ideal, utilizando as seguintes fórmulas: • para homens: (72.7 * H) - 58 • para mulheres: (62.1 * H) – 44.
+
 $altura= 1.70;
 $sexo= 'f';
 
