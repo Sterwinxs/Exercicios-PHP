21e7420dd85f4c01b24666de07896eb2dcfe88c0
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 08:52:26 -0300

    Update Exercicio 1.php

--- Exercicio 1.php
+++ Exercicio 1.php
@@ -1,10 +1,12 @@
 <?php
+//Efetue um algoritmo PHP que receba um valor e imprima o  texto "o valor é maior que 10" caso isso seja verdade, senão imprima "o valor é menor que 10".
+
 $num= 10;
 
 if ($num >= 10){
-    echo 'Numero maior ou igual a 10!';
+    echo 'O valor é maior ou igual a 10!';
 }elseif ($num <10){
-    echo 'Numero menor que 10!';
+    echo 'O valor é menor que 10!';
 }
 
 
