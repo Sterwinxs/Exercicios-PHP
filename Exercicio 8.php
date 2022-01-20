874b66c5da2d6d2ce51d8c387f28d5e8db7b9968
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 09:02:12 -0300

    Update Exercicio 8.php

--- Exercicio 8.php
+++ Exercicio 8.php
@@ -1,4 +1,6 @@
 <?php 
+//Efetue um algoritmo PHP que calcule e imprima o salário reajustado de um funcionário de acordo com a seguinte regra: • salários até 300, reajuste de 50% • salários maiores que 300, reajuste de 30%
+
 $salario= 310;
 
 if ($salario <= 300){
