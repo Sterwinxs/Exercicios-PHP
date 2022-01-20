cc4b6139a736087901912b3ea24e2ac05bc22d25
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 12:13:22 -0300

    Add files via upload

---
+++ Exercicio 14.php
@@ -0,0 +1,16 @@
+<?php
+//Efetue um algoritmo PHP que receba duas strings, e descubra se elas são iguais ou diferentes, imprimindo “IGUAL” ou “DIFERENTE” respectivamente.
+
+$nome1= 'Eduarda';
+$nome2= 'Fernanda';
+
+if ($nome1 == $nome2){
+    echo 'Os nomes são iguais!';
+}else{
+    echo 'Os nomes não são iguais!';
+}
+    
+	
+
+
+?>
