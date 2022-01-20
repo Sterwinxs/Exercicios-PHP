56e14bc0286b90a2c0809ef65580b910a70f91ae
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 08:57:26 -0300

    Update Exercicio 5.php

--- Exercicio 5.php
+++ Exercicio 5.php
@@ -1,4 +1,6 @@
 <?php
+//Efetue um algoritmo PHP que receba os valores A, B e imprima-os em ordem crescente em relação aos seus valores. Exemplo, para A=5, B=4. Você deve imprimir na tela: "4 5".
+
 $valorA= 5;
 $valorB= 3;
 
