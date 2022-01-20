2ea761a7c6f230fa35dc2f31d4b8d8312136f19f
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 11:01:14 -0300

    Add files via upload

---
+++ Exercicio 11.php
@@ -0,0 +1,17 @@
+<?php
+//Efetue um algoritmo PHP que receba dois valores quaisquer e imprime todos os valores intermediários a ele, veja exemplo: Primeiro Valor = 5 Segundo Valor = 15 Imprime: 6 7 8 9 10 11 12 13 14
+
+$valor1= 5;
+$valor2= 15;
+$valorintermediario= '6, 7, 8, 9, 10, 11, 12, 13, 14';
+
+if($valor1 > $valor2){
+    echo $valor1;
+}else{
+    echo $valorintermediario;
+}
+    
+
+
+
+?>
