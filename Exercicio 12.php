dedec0f9c8ff388e97a6a0795d631b2afbedfed3
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 11:14:15 -0300

    Add files via upload

---
+++ Exercicio 12.php
@@ -0,0 +1,11 @@
+<?php
+//Efetue um algoritmo PHP que receba dois valores quaisquer e efetue sua multiplicação utilizando para isso apenas o operador “+”, visto que: (3 * 5) = 5 + 5 + 5 (4 * 12) = 12 + 12 + 12 + 12
+  $valor1= 5;
+  $valor2= 12;
+
+  $resultado= $valor2+$valor2+$valor2+$valor2+$valor2;
+
+  echo $resultado;
+	
+
+?>
