2e8e9a637d8086a3a7016c1a1ccfa99e795d8723
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 10:45:05 -0300

    Update Exercicio 10.php

--- Exercicio 10.php
+++ Exercicio 10.php
@@ -1,6 +1,5 @@
 <?php
 //Efetue um algoritmo PHP que receba um valor qualquer e calcule o seu fatorial (!), sabendo que fatorial de um número é: 7! = 7*6*5*4*3*2*1 4! = 4*3*2*1
-
 $valor=8;
 
 $fatorial=$valor*7*6*5*4*3*2*1;
