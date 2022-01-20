742643ba9849e54bcb8907966748ea0f7b683fa9
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 14:46:10 -0300

    Add files via upload

---
+++ Exercicio 15.php
@@ -0,0 +1,11 @@
+<?php
+//Efetue um algoritmo PHP que receba uma string e um caractere de busca e  descubra o número de ocorrências deste caractere dentro da string. Para isso  utilize as funções de string “strlen".
+$algoritmo= 'Quantos caracteres tem nessa string?';
+
+$quantidade= strlen($algoritmo);
+
+echo $quantidade;
+
+
+
+?>
