3fc21364f2b61916ebc1a3135cee6198bed96768
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 08:56:03 -0300

    Update Exercicio 4.php

--- Exercicio 4.php
+++ Exercicio 4.php
@@ -1,4 +1,6 @@
 <?php
+//Efetue um algoritmo PHP que receba os valores A, B, C e D. Efetue a soma entre A e C, a multiplicação entre B e D e verifique se o resultado da soma é maior, menor ou igual ao da multiplicação. Imprima: "A+C é maior que B+D", "A+C é menor que B+D", "A+C é igual a B+D".
+
 $valorA= 1;
 $valorB= 2;
 $valorC= 3;
