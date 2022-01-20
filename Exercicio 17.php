24921962090e0f2c2bc7980659fc7a1f3a40c3e2
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-20 15:54:07 -0300

    Add files via upload

---
+++ Exercicio 17.php
@@ -0,0 +1,19 @@
+<?php
+//Entrar com um número e informar se ele é divisível por 10, por 5, por 2 ou se não é divisível por nenhum destes
+$valor= 9;
+
+if ($valor%10 == 0){
+    echo 'O valor é divisivel por 10<br>';
+}elseif ($valor%5 == 0 ){
+    echo 'O valor é divisivel por 5';
+}elseif ($valor%2 == 0){
+    echo 'O valor é divisivel por 0';
+}else{
+    echo 'O valor não é divisivel por por nenhum destes!';
+}
+
+
+
+
+
+?>
