4a9ee4e401c45ee008f802b970dd85e430dfa6a2
Gabrielly <96553555+Sterwinxs@users.noreply.github.com>
2022-01-21 12:49:40 -0300

    Add files via upload

---
+++ Exercicio 25.php
@@ -0,0 +1,12 @@
+<?php
+//Entrar com 20 números e imprimir a soma dos positivos e o total de números negativos
+
+$numeros= array (1,10,23,26,-15,8,-35,-5,3,14,7,-3,6,4,8,13,24,-11,7,4);
+
+echo  'soma dos valores positivos= ';
+echo $numeros[0] + $numeros[1]+$numeros[2]+$numeros[3]+$numeros[5]+$numeros[8]+$numeros[9]+$numeros[10]+$numeros[12]+$numeros[13]+$numeros[14]+$numeros[15]+$numeros[16]+$numeros[18]+$numeros[19];
+echo '<br>';
+echo 'O total de números negativos é 5.';
+
+
+?>
